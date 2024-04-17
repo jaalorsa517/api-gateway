e7538f1f9fe077d4364a95dad3a59879e8b54a98
jaalorsa517 <jaalorsa517@hotmail.com>
2024-04-17 13:21:55 -0500

    Subiendo version a function

--- function/server_function.ts
+++ function/server_function.ts
@@ -1,4 +1,4 @@
-import { https } from "firebase-functions"
+import { onRequest } from "firebase-functions/v2/https"
 import { app } from "./api-gw/index.js"
 
-export const apigw = https.onRequest(app)
+export const apigw = onRequest(app)
